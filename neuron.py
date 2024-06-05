416b9bf8cb5c24ab99b8e0ae35974d2dfd651b0c
pontusblomqvist <pontus.blomqvist94@gmail.com>
2024-06-05 17:17:07 +0200

    cleanup for easier reading

--- neuron.py
+++ neuron.py
@@ -6,20 +6,8 @@ class Neuron:
         self.w = np.random.randn(n_inputs) * 0.1
         self.b = np.random.randn() * 0.1
 
-    def sigmoid_activation(self, n):
-        return 1 / (1 + np.exp(-n))
-    
-    def sigmoid_derivative(self, n):
-        return n * (1 - n)
-    
-    def binary_cross_entropy_loss(self, prediction, target):
-        return -target * np.log(prediction) - (1 - target) * np.log(1 - prediction)
-
-    def binary_cross_entropy_loss_derivative(self, prediction, y):
-        return -y / prediction + (1 - y) / (1 - prediction)
-
     def backpropagation(self, X, y, prediction, learning_rate):
-        bias_gradient = self.sigmoid_derivative(prediction) * self.binary_cross_entropy_loss_derivative(prediction, y)
+        bias_gradient = sigmoid_derivative(prediction) * binary_cross_entropy_loss_derivative(prediction, y)
         weight_gradients = np.array([x * bias_gradient for x in X])
         self.w -= learning_rate * weight_gradients
         self.b -= learning_rate * bias_gradient
@@ -35,6 +23,19 @@ class Neuron:
                 print(f"Epoch: {epoch}, Loss: {losses.mean()}")
 
     def forward_pass(self, X, y=None):
-        prediction = self.sigmoid_activation(np.dot(self.w, X) + self.b)
-        loss = self.binary_cross_entropy_loss(prediction, y) if y is not None else None
+        prediction = sigmoid_activation(np.dot(self.w, X) + self.b)
+        loss = binary_cross_entropy_loss(prediction, y) if y is not None else None
         return prediction, loss
+
+
+def sigmoid_activation(n):
+        return 1 / (1 + np.exp(-n))
+    
+def sigmoid_derivative(n):
+    return n * (1 - n)
+
+def binary_cross_entropy_loss(prediction, target):
+    return -target * np.log(prediction) - (1 - target) * np.log(1 - prediction)
+
+def binary_cross_entropy_loss_derivative(prediction, y):
+    return -y / prediction + (1 - y) / (1 - prediction)
